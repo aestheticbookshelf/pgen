ca4f1c81e728f0c8458ad269c6ad45e69e9a0311
aestheticbookshelf <aestheticbookshelf@gmail.com>
2020-02-19 21:51:40 +0100

    Write files

--- pgen.js
+++ pgen.js
@@ -1,6 +1,8 @@
 const path = require('path')
 const jsonfile = require('jsonfile')
 const fs = require('fs')
+const rimraf = require('rimraf')
+const { execSync } = require('child_process')
 
 console.log()
 
@@ -25,18 +27,66 @@ function createConfig(props){
     return config
 }
 
+function createPackageJson(props){
+    let pkgJson = {
+        name: `@${props.npmUser}/${props.repo}`,
+        version: `1.0.1`,
+        description: props.description,
+        main: props.mainPath,
+        scripts: {"test": "echo \"Error: no test specified\" && exit 1"},
+        keywords: props.keywords,
+        author: props.gitHubUser,
+        license: props.license || "MIT",
+        homepage: `https://github.com/${props.gitHubUser}/${props.repo}`,
+        bugs: {
+            url: `https://github.com/${props.gitHubUser}/${props.repo}/issues`,
+            email: props.gitHubUserMail
+        },
+        repository: {
+            type: "git",
+            url: `https://github.com/${props.gitHubUser}/${props.repo}.git`
+        },
+        dependencies: {},
+    }
+
+    return pkgJson
+}
+
 jsonfile.readFile('conf.json').then(obj => {
     let make = obj.make
-    let root = path.join(__dirname, obj.common.rootDirRel, make)
+    let root = path.join(__dirname, obj.common.rootDirRel || "../", make)
     console.log("making", make)    
     try{fs.mkdirSync(root);console.log("created", root)}catch(err){console.log(root, "already exists")}
-    let props = {...obj.common, ...obj[make]}
+    let props = {...obj.common, ...obj.packages[make]}
+    props.repo = make
+    props.main = props.main || `${props.repo}.js`
+    props.srcDir = props.srcDir || "src"
+    props.mainPath = `${props.srcDir}/${props.main}`
+
     console.log("writing .gitignore")
     fs.writeFileSync(path.join(root, ".gitignore"), `\nnode_modules\n`)
     console.log("writing config")
-    fs.writeFileSync(path.join(root, "config"), createConfig({
-        gitHubUser: props.gitHubUser,
-        gitHubUserMail: props.gitHubUserMail,
-        repo: make,
-    }))
+    fs.writeFileSync(path.join(root, "config"), createConfig(props))
+    console.log("removing git")
+    rimraf.sync(path.join(root, ".git"))
+    console.log("initializing git")
+    execSync('git init', {
+        cwd: root
+    })
+    console.log("writing config")
+    fs.copyFileSync(path.join(root, "config"), path.join(root, ".git/config"))    
+    console.log("writing package.json")
+    fs.writeFileSync(path.join(root, "package.json"), JSON.stringify(createPackageJson(props), null, 2))
+    console.log("writing LICENSE")
+    fs.copyFileSync(path.join(__dirname, "LICENSE"), path.join(root, "LICENSE"))    
+    let sRootSrc = path.join(__dirname, "s")
+    let sRoot = path.join(root, "s")
+    console.log("writing scripts")
+    try{fs.mkdirSync(sRoot);console.log("created", sRoot)}catch(err){console.log(sRoot, "already exists")}    
+    fs.copyFileSync(path.join(sRootSrc, "init.bat"), path.join(sRoot, "init.bat"))    
+    fs.copyFileSync(path.join(sRootSrc, "c.bat"), path.join(sRoot, "c.bat"))    
+    fs.copyFileSync(path.join(sRootSrc, "p.bat"), path.join(sRoot, "p.bat"))   
+    console.log("creating src")    
+    if(props.srcDir != ".") try{fs.mkdirSync(path.join(root, props.srcDir));console.log("created", props.srcDir)}catch(err){console.log(props.srcDir, "already exists")}    
+    fs.writeFileSync(path.join(root, props.mainPath), "")
 })
